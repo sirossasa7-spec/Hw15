75ebdaac57ae17231361e3ad55eeced19c17f1fc
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-07-14 21:08:34 +0200

    Task4

---
+++ Hw-15/Task3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Hw_15
+{
+    class Article
+    {
+        public string Title { get; set; }
+        public int Symbols { get; set; }
+        public string Preview { get; set; }
+    }
+
+    class Magazine
+    {
+        public string Name { get; set; }
+
+        public List<Article> Articles { get; set; } = new List<Article>();
+    }
+    internal class Task3
+    {
+        static void Main()
+        {
+            Magazine m = new Magazine();
+
+            m.Name = "IT News";
+
+            m.Articles.Add(new Article
+            {
+                Title = "C#",
+                Symbols = 5000,
+                Preview = "Основи C#"
+            });
+
+            m.Articles.Add(new Article
+            {
+                Title = "LINQ",
+                Symbols = 3500,
+                Preview = "Запити LINQ"
+            });
+
+            string json = JsonSerializer.Serialize(m);
+
+            File.WriteAllText("magazine.json", json);
+
+            Magazine mag = JsonSerializer.Deserialize<Magazine>(File.ReadAllText("magazine.json"));
+
+            Console.WriteLine(mag.Name);
+
+            foreach (var a in mag.Articles)
+            {
+                Console.WriteLine($"{a.Title} {a.Symbols}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Hw-15/Task4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Hw_15
+{
+    class Magazine
+    {
+        public string Name { get; set; }
+        public int Pages { get; set; }
+    }
+    internal class Task4
+    {
+        static void Main()
+        {
+            List<Magazine> magazines = new List<Magazine>()
+            {
+                new Magazine{ Name="IT News", Pages=120},
+                new Magazine{ Name="Sport", Pages=80},
+                new Magazine{ Name="Science", Pages=150}
+            };
+
+            string json = JsonSerializer.Serialize(magazines);
+
+            File.WriteAllText("magazines.json", json);
+
+            List<Magazine> list =
+                JsonSerializer.Deserialize<List<Magazine>>(File.ReadAllText("magazines.json"));
+
+            foreach (var m in list)
+            {
+                Console.WriteLine($"{m.Name} - {m.Pages}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
